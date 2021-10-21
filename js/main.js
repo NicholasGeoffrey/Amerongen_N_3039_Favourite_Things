26b2ebf56bca006cfe66a3a588daf42b4fb37c7b
Bitchmcgee <sharktacularsonny@gmail.com>
2021-10-21 15:01:23 -0400

    Sacrificed Icon for working code

--- js/main.js
+++ js/main.js
@@ -1,157 +1,80 @@
 (()=> {
-    const theTeam = document.querySelector("#favourite-things"),
-    theTemplate = document.querySelector("#bio-template").content; 
+   // const   theTeam = document.querySelector("#favourite-things"),
+     //       theTemplate = document.querySelector("#bio-template").content; 
 
-    let buttons = document.querySelectorAll("button");
+    const btn = document.querySelectorAll(".btn");
+    let thingsList;
 
-    function buildControls(info) {
-        info.forEach(things =>{
-            let panel = theTemplate.cloneNode(true),
-            display = panel.firstElementChild.children;
-
-            panel.firstElementChild.dataset.key = things.id;
-
-            display[3].querySelector('img').src = `images/${info[panel].poster}`;
-        display[0].textContent = things[key].title;
-        display[1].textContent = things[key].media;
-        display[2].textContent = things[key].description
-
-        theTeam.appendChild(panel);
-        })
-    }
-
-    function showData(event) {
-        let key = this.dataset.key;
-        let display = document.querySelector("h1");
-
-        display[3].querySelector('img').src = `images/${info[panel].poster}`;
-        display[0].textContent = things[key].title;
-        display[1].textContent = things[key].media;
-        display[2].textContent = things[key].description
-
-        showData({id: key});
-    }
     function getData() {
         //use the Fetch API to retrieve some data
         fetch("./data.json")
         .then(res => res.json()) // unpack the API response (turn it back into plain js)
         .then(data => {
-            console.log(data);
-            // getData = data;
-            buildControls(data);
-          //  displayStuff = data;
-            // do anything else here that we want with our data
-            //call a function to generate our dynamic content
            
+
+            // Step 2
+            // save your data into the variable you set up above (like this: things = data)
+            thingsList = data;
+           return thingsList;
+            // buildControls(data);    
+                  
         })
+        .catch(error => console.error(error));
     }
-/*           
+
+    function buildControls(data) {
+        // you would just create your three buttons here - one for each "thing" in your data.json object
+        // we can work on this after you get the data showing up - it's hard-coded into your HTML so no need to do this right now
+        let thingsList = Object(data);
+        /* console.log("build your buttons when this function fires - we'll do this later");
+        showData(); */
+    }
+
+    // Step 1
+    // add a variable here to store the data you retrieve in your fetch call (something like this: things = {})
     
-    let buttons = document.querySelectorAll("button")      
-    var displayStuff;
-        // things = {};
+    
 
-       function showData() {
-           let key = this.dataset.key;
-           let display = document.querySelector("h1");
+    function showData(event) {
+        // key is the reference to the piece of the data object you're using
+        // as an example when you click on the Hamilton button, then you want the info from the
+        // Hamilton object inside your data.json file
 
-           display.textContent = things[key].name;
-       } 
-    //start with fetch call
-    */
-/*     function getData() {
-        //use the Fetch API to retrieve some data
-        fetch("./data.json")
-        .then(res => res.json()) // unpack the API response (turn it back into plain js)
-        .then(data => {
-            console.log(data);
-            getData = data;
-            displayStuff = data;
-            // do anything else here that we want with our data
-            //call a function to generate our dynamic content
-           
-        })
-.catch(error => console.error(error)); //catch and report any errors
+        let key = event.target.dataset.list;
+        console.log(key)
+        buildControls();
+        console.log(thingsList);
+        // you want to now select the bio panel from index.html, not just the headline
+        // and change its content depending on which button you click
+        let thingTitle = document.querySelector("h2"),
+        thingPoster = document.querySelector("img"),
+        thingMedia = document.querySelector("h3"),
+        thingDescription = document.querySelector("p");
 
-    } */
-
-   /*  function buildTeam(info) {
-        // first, get a reference to all of the people (the keys of the info object)
-        let team = Object.keys(info);
+    thingPoster.src = `images/${thingsList[key].poster}`;
+    thingTitle.textContent = thingsList[key].title;
+    thingMedia.textContent = thingsList[key].media;
+    thingDescription.textContent = thingsList[key].description;
+    
+    
 
 
-        team.forEach(person => {
-            let panel = theTemplate.cloneNode(true),
-                memberInfo = panel.firstElementChild.children;
+     
+        /*   let thingPanel = document.querySelector("#bio-panel").children;
 
-            memberInfo[0].querySelector('img').src = `images/${info[panel].poster}`;
-            memberInfo[1].textContent = info[panel].title;
-            memberInfo[2].textContent = info[panel].media;
-            memberInfo[3].textContent = info[panel].description;
+        thingPanel[0].querySelector('img').src = `images/${things[key].poster}`;
+        thingPanel[1].textContent = things[key].title;
+        thingPanel[2].textContent = things[key].media;
+        thingPanel[3].textContent = things[key].description */
 
-            //put the virtual panel in the tean section in our HTML page
-            theTeam.appendChild(panel);
-            theTeam.appendChild(memberInfo);
-        })
     }
-        // next, clone the template because we can't use it directly
-        debugger;
-    } */
-     /*  function showFavourites(info) {
-          info.forEach(thing => {
-          let display = theTemplate.cloneNode(true),
-          thingDisplay = display.firstElementChild.children;
-            display,firstElementChild.dataset.key = thing.id;
-          let key = this.dataset.key;
-          //let thingDisplay = document.querySelector("favourite-things")
-          thingDisplay[0].textContent = data[key].title;
-          thingDisplay[1].textContent = data[key].media;
-          thingDisplay[2].textContent = data[key].description;
-          thingDisplay[4].querySelector('img').src = `images/${data[key].poster}`;
 
-          theTeam.appendChild(display);
-      })} */
-   /*  function expandTeam(info) {
-        // first, get a reference to all of the people (the keys of the info object)
-        let team = Object.keys(info);
-        if (theTemplate.children.length > 0) {
-            theTemplate.innerHTML = "";
-        }
-
-        team.forEach(person => {
-            let panel = theTemplate.cloneNode(true),
-                memberInfo = panel.firstElementChild.children;
-
-           
-             memberInfo[1].textContent = info[person].title;
-             memberInfo[2].textContent = info[person].media;
-             memberInfo[3].textContent = info[person].description;
-
-            //put the virtual panel in the tean section in our HTML page
-            theTeam.appendChild(panel);
-        })
-        // next, clone the template because we can't use it directly
-        debugger;
-    }   
-   // function selectThing(info) {
-   //    if (dataDisplay.children.length > 0) {
-    //        dataDisplay.innerHTML = "";
-    //    }
-    //    let panel = theTemplate.cloneNode(true),
-    //    memberInfo = panel.firstElementChild.children;
-
- //       memberInfo[1].textContent = info[person].title;
-   //     memberInfo[2].textContent = info[person].media;
-     //   memberInfo[3].textContent = info[person].description;
-   // } panel.forEach(template => template.addEventlistener("click", selectThing));
-    // invoke the getData function
+   
     getData();
-*/
-buttons.forEach(button => button.addEventListener("click", buildControls));
+    // when you click on a button, you want to show that "thing"'s data, not build controls
+    btn.forEach(button => button.addEventListener("click", showData));
 
-getData(null, buildControls);
-   // selectThing();
+    // dont' need to pass arguments here because the getData function isn't expecting any - it's in this file
+   
  
-}
-
-)()
+})();
